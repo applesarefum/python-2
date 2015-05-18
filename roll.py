27b6b3c3009a532f61fbe83e3895589ba43de6e6
William B. Muhlbach <applesarefum@gmail.com>
2015-05-18 16:17:47 -0400

    save

--- roll.py
+++ roll.py
@@ -1,15 +1,25 @@
 import random as rand
-def roll1(sides):
+def rolldie(sides=6):#unless other variable is defined, use six sides
     return rand.randint(1,sides)
-def roll(times,sides):
+def roll(times=1,sides=6):
     total=0
     for count in range(times):
-        total+=roll1(sides)
+        total+=rolldie(sides)
     return total
 
 if __name__=='__main__':
-    qwe=roll1(7)
+    import colors as c
+    print(c.orange)
+    print('rolldie(13) ran 8 times results in:')
+    for count in range(7):
+        qwe=rolldie(13)
+        print(qwe,end=', ')
+    qwe=rolldie(13)
     print(qwe)
+    print('roll(3,6) ran 5 times results in:')
+    for count in range(4):
+        qwe=roll(3,6)
+        print(qwe,end=', ')
     qwe=roll(3,6)
     print(qwe)
 #    def varin():
