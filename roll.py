27935f8a4e26167c3325156bf9196c6230980ae4
William B. Muhlbach <applesarefum@gmail.com>
2015-05-20 20:24:40 -0400

    save

--- roll.py
+++ roll.py
@@ -1,55 +1,40 @@
 """Library for dice rolling"""
 import random as rand
-def rolldie(sides=6):#unless other variable is defined, use six sides
-    """qwe"""
-    return rand.randint(1,sides)
 
 def roll(times=1,sides=6):
     """qwe"""
     total=0
     for count in range(times):
-        total+=rolldie(sides)
+        total+=rand.randint(1,sides)
     return total
 
 def parse(text):#text should be something like '2d5'
-    """Parses traditional dicce notation(ex: '2d5' returns 2,5)"""
+    """Parses traditional dice notation(ex: '2d5' returns 2,5)"""
     times,sides=text.split('d')
     return int(times),int(sides)
 
-def parse_roll(text):#text should be something like '2d5
+def parse_roll(text):#text should be something like '2d5'
     """Parses traditional dice notation and rolls(ex: 2d5 could return 8)"""
     (times,sides)=parse(text)
     return roll(times,sides)
 
 if __name__=='__main__':
     import colors as c
-    print(c.orange+'rolldie(13) ran 8 times results in:')
-    for count in range(7):
-        qwe=rolldie(13)
-        print(qwe,end=', ')
-    qwe=rolldie(13)
-    print(qwe)
+    print(c.magenta+'\b')
     print('roll(3,6) ran 5 times results in:')
     for count in range(4):
         qwe=roll(3,6)
         print(qwe,end=', ')
     qwe=roll(3,6)
     print(qwe)
-    print('parse_roll(2d5) returns:')
+    
+    print('parse(2d5) returns:')
     qwe=parse('2d5')
     print(qwe)
+    
     print('parse_roll(2d5) ran 4 times results in:')
     for count in range(3):
         qwe=parse_roll('2d5')
         print(qwe,end=', ')
     qwe=parse_roll('2d5')
     print(qwe)
-#    def varin():
-#        
-#        bar=input('How many sides would you like this die to have? ')
-#        bar=int(bar)
-#        qwe=input('How many times would you like to roll this die? ')
-#        qwe=int(qwe)
-#        foo=roll(qwe,bar)
-#        print(foo)
-
