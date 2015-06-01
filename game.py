ff1b5915b6b315f0a50df99d38e61294bb74f7ee
William B. Muhlbach <applesarefum@gmail.com>
2015-06-01 17:18:00 -0400

    save

---
+++ game.py
@@ -0,0 +1,13 @@
+from player import Player
+class Game():
+    def __init__(self):
+        print('welcom 2 teh gaem')
+    
+    def start(self):
+        #TODO show stats
+        self.start()
+    def choosemob():
+        #TODO choose monster
+if __name__=='__main__':
+    qwer=Game()
+    qwer.start()
